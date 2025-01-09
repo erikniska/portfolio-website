b82a4764ecaf886bf499c948359fc93704fe5791
erikniska <erik.niska@hotmail.com>
2025-01-09 15:18:08 +0100

    Nystart och nybygge samt formatering

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Main from './views/main-page/Main';
+import './assets/colors.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/views/main-page/Main.js
+++ src/views/main-page/Main.js
@@ -2,19 +2,24 @@ import React from 'react';
 import './Main.css'; 
 
 const Main = () => {
-  const image = require('../../assets/images/background-image.jpg');  
-
-
+  
   return (
     <div>
-      <div className="block-header">
-        <img src={image} alt="background-image" />
-        <div className="text-overlay">
-          <p>BLOCKTEXT</p>
-        </div>
+      <div className="margin-header">
       </div>
-      <div className="block-content">
-        <p>TEXT</p>
+      <div className="block-header-wrapper">
+        <div className="block-header">
+           <div className='grid-box'>
+              <p>Hej! Mitt namn är</p> 
+              <h1>ERIK NISKA</h1>
+              <h2>Välkommen till min hemsida!</h2>
+            </div>
+          </div>
+        </div>
+      <div className="block-content-wrapper">
+        <div className="block-content">
+            <p>TEXT</p>
+        </div>
       </div>
     </div>
   );
